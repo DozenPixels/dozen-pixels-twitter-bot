425ef99f14ad6b327c8a9fb8fccee101c46361eb
Dozen Pixels <hello@dozenpixels.com>
2017-01-31 03:38:56 +0000

    added env. vars

--- config.js
+++ config.js
@@ -7,18 +7,18 @@
 
 // Uncomment for when deploying to Heroku
 
-// module.exports = {
-//   consumer_key: process.env.CONSUMER_KEY,
-//   consumer_secret: process.env.CONSUMER_SECRET,
-//   access_token: process.env.ACCESS_TOKEN,
-//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-// };
-
 module.exports = {
-  consumer_key: 'jvGlyquclR9I1hBNer5YTiNnz',
-  consumer_secret: 'ynmEjWE74VI5bdVEe6uAaaW6RBVAd7ShvVMFRCvwDFBsk3Femb',
-  access_token: '814622913097822208-qhLbLGj3fiZSMkP3nfgBo61EYZZTnd7',
-  access_token_secret: 'KGsICp2shE5Uhe8SyGpwSXz31U7sWP1TeVh4IkpWNejl5',
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 };
+
+// module.exports = {
+//   consumer_key: '',
+//   consumer_secret: '',
+//   access_token: '',
+//   access_token_secret: '',
+// };
 
 
